3fc95b6ab90d800892ef6a29e07639b5e90197f1
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2023-08-13 17:50:26 -0400

    probando cambios en clases

--- clases/pruebas_clases/fibo.c
+++ clases/pruebas_clases/fibo.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i, n, act = 1, ant=0, sig=1;
-    printf("\nEste programa muestra la sucesión de Fibonacci hasta el valor n ingresado");
+    printf("\nEste programa muestra los valores de la sucesión de Fibonacci hasta el valor n ingresado");
     printf("\nIngrese el valor de n, natural: ");
     scanf("%i", &n);
     for (i = 0; i < n; i++)
