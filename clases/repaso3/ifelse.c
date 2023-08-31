d80392a0bcc27405617af35c42dd1991ad74cd12
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2023-08-31 15:29:34 -0400

    update

--- clases/repaso3/ifelse.c
+++ clases/repaso3/ifelse.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int a;
+    float r;
     for (a = 12; a > 1; a--)
     {
         printf("\nVoy en el %i\n", a * a);
     }
     printf("\n\n");
+    r=sqrt(a);
     return 0;
 }
