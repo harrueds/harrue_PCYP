42b901c4fda7cef92e77fe8e3257cd7e91238b88
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2023-09-04 08:35:22 -0300

    UPDATE

--- clases/pruebas_clases/nochar.c
+++ clases/pruebas_clases/nochar.c
@@ -3,19 +3,21 @@
 int main() {
     float numero;
     char caracter;
-    int intentos = 3;
+    int intentos = 4;
 
     printf("Ingrese un número decimal (float): ");
 
-    while (scanf("%f", &numero) != 1) {
+    while (scanf("%f", &numero) != 1)
+    {
         while (getchar() != '\n'); // Limpiar el búfer de entrada
 
         if (intentos == 1) {
             printf("Has agotado todos los intentos. El programa se cerrará.\n");
             return 1; // Salir del programa con código de error 1
-        }else if(intentos ==2)
+        }
+        else if(intentos ==2)
         {
-            printf("\nLe queda un intento antes de cerrar el programa\n\n");
+            printf("\nQUEDA UN INTENTO ANTES DE CERRAR EL PROGRAMA\n\n");
         }
 
         printf("Error: Ha ingresado un carácter no válido. Intente nuevamente.\n");
