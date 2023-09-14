b9291dd78abb097e66f34a3643979e5442dbc211
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2023-09-14 12:55:13 -0300

    update

--- lengC/basula/cuadratica.c
+++ lengC/basula/cuadratica.c
@@ -14,12 +14,12 @@ int main()
     printf("\nEl discriminante es %.2f",disc);
     if(disc<0)
     {
-        printf("\nAl ser negativo el discriminante, no existe solución real");
+        printf("\nAl ser negativo el discriminante, no existe solución real\n\n\n");
     }
     else if(disc==0)
     {
         x=-b/(2*a);
-        printf("\El discriminante es cero, por lo que existe una solucion real.");
+        printf("\nEl discriminante es cero, por lo que existe una solucion real.");
         printf("\nLa solución es: %.2f\n\n\n",x);
     }
     else
@@ -27,7 +27,7 @@ int main()
         x1=(-b-pow(disc,0.5))/(2*a);
         x2=(-b+pow(disc,0.5))/(2*a);
         printf("\nEl discriminante es positivo, por lo que existen dos soluciones:");
-        printf("\nLas soluciones son:\n x1=%.2f \t x2=%.2f",x1,x2);
+        printf("\nLas soluciones son:\n x1=%.2f \t x2=%.2f\n\n\n",x1,x2);
     }
     return 0;
 }
