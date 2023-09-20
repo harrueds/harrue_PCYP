8c5173a5bb5fa079cfd878de41e37b55ab250453
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2023-09-20 16:14:52 -0300

    update

--- pythonizado/raiz.py
+++ pythonizado/raiz.py
@@ -1,4 +1,4 @@
 #calcula la raíz cuadrada de un número ingresado por teclado
 numero = int(input("Ingrese un número positivo para calcular su raíz "))
 raiz = numero**0.5
-print(f"La raíz de {numero} es {raiz}\n")
+print("La raíz de ",numero," es {:.2f}".format(raiz))
